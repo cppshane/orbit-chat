b0214889521386b504d032ecd93c8c4d98cfca53
Shane Duffy <shane@shaneduffy.io>
2022-08-20 19:00:39 -0700

    Remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IpfsService } from './services/ipfs/ipfs.service';
 import { OrbitdbService } from './services/orbit-db/orbitdb.service';
 
